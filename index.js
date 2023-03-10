c0d62e9446e6950526a76c4dcf69cbaf19fdef1d
SeanQuinn <Seanquinn917@gmail.com>
2023-03-10 10:23:20 -0500

    commit tree 02fb3db5cb1b7ebfc3bf0ce2382880e723b7e454

--- index.js
+++ index.js
@@ -21,3 +21,93 @@ const allWagesFor = function () {
     return payable
 }
 
+function createEmployeeRecord([string1, string2, string3, num]){
+let employeeRecordObj = {
+    firstName: string1,
+    familyName: string2,
+    title: string3,
+    payPerHour: num,
+    timeInEvents: [],
+    timeOutEvents: []
+}
+return employeeRecordObj
+}
+
+function createEmployeeRecords(Array){
+    let newArray = Array.map(createEmployeeRecord)
+    return newArray
+}
+
+let employeeRecordObj =  {
+    firstName: string1,
+    familyName: string2,
+    title: string3,
+    payPerHour: num,
+    timeInEvents: [],
+    timeOutEvents: []
+}
+// function createTimeInEvent(dateStamp, employeeRecordObj){
+//     //console.log("this is what i am looking for", dateStamp)
+    
+    
+//     let dateStampArray = dateStamp.split(' ')
+//     let parsedTime = parseInt(dateStampArray[1], 10)
+//     console.log(dateStampArray, '<---look! there it is!')
+//     let timeInObj = {
+//         type: 'TimeIn',
+//         hour: parsedTime,
+//         date: `${dateStampArray[0]}`
+//     };
+//     console.log(timeInObj, 'yahoo!!')
+//     employeeRecordObj.timeInEvents.push(timeInObj)
+//     return employeeRecordObj
+// }
+
+// createEmployeeRecord.call()
+function createTimeInEvent(dateStamp){
+   
+    let dateStampsArray = dateStamp.split(" ");
+  
+    let parsedNum = parseInt(dateStampsArray[1], 10)
+    //console.log(parsedNum)
+    let timeInEventObj = {
+    type: "TimeIn",
+    hour: parsedNum,
+    date: `${dateStampsArray[0]}`
+  };
+    //console.log("it should be here", timeInEventObj)
+    this.timeInEvents.push(timeInEventObj)
+    
+    return this
+  };
+
+  function createTimeOutEvent(dateStamp){
+    let dateStampArray = dateStamp.split(" ")
+    let parsedNum = parseInt(dateStampArray[1], 10)
+    let timeOutObj = {
+        type: 'TimeOut',
+        hour: parsedNum,
+        date:`${dateStampArray[0]}`
+    }
+    this.timeOutEvents.push(timeOutObj)
+    return this
+  }
+
+//   function hoursWorkedOnDate(dateStamp){
+//     let dateStampArray = dateStamp.split(' ')
+//     const target = `${dateStampArray[0]}`;
+//     let timeIn = this.timeInEvents.find(ob => {
+//         for(key in target) {
+//             if (!(key in ob)) return false;
+//             if (ob[key] !== target[key]) return false
+//         }
+//         return true
+//     })
+   
+//     console.log("lolok!", timeIn)
+//     //let timeOut = this.timeOutEvents.find(createTimeOutEvent)
+//   }
+
+function hoursWorkedOnDate(dateStamp){
+console.log("ehllo!", this.timeInEvents)
+}
